f6b3ebc54a82edbb1dfca46f5abb621a3ce559ca
MarioAguirre <MarioAguirre@192.168.1.108>
2021-03-15 08:24:15 +0100

    15/03/2021 - CASA

--- src/test/java/Tienda/UsuarioTest.java
+++ src/test/java/Tienda/UsuarioTest.java
@@ -25,7 +25,6 @@ public class UsuarioTest {
     @DisplayName("Test de Comprobacion del Nombre I")
     public void testComprobacionNombre() {
         String nombre = "hhhhhh";
-//        Usuario yo = new Usuario("hhhhhh","Marioaguirresanz","mario@eepmad.com");
         boolean expResult = true;
         boolean result = Usuario.comprobacionNombre(nombre);
         assertEquals(expResult, result);
@@ -37,8 +36,7 @@ public class UsuarioTest {
     @Test
     @DisplayName("Test de Comprobacion de la Contraseña I")
     public void testComprobacionPassword() {
-        String pss = "Marioaguirresanz";
-//        Usuario yo = new Usuario("MarioAguirre","Marioaguirresanz","mario@eepmad.com");
+        String pss = "Marioaguirresanz";    
         boolean expResult = true;
         boolean result = Usuario.comprobacionPassword(pss);
         assertEquals(expResult, result);
@@ -51,7 +49,6 @@ public class UsuarioTest {
     @DisplayName("Test de Comprobacion del Mail I")
     public void testComprobacionMail() {
         String contr = "mario@eepmad.com";
-//        Usuario yo = new Usuario("hhhhhh","Contrseña","mario@eepmad.com");
         boolean expResult = true;
         boolean result = Usuario.comprobacionMail(contr);
         assertEquals(expResult, result);
